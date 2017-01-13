7f6e9c029a0d4fff27dbabac9862d9339117a9d1
Tatiana Limache Quispe <Tati34che@gmail.com>
2017-01-13 18:55:10 -0500

    Valide el input

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+function verifica(evt)
+{
+  if(window.event.keyCode >='48' && window.event.keyCode<='57' || window.event.keyCode =='8' )
+    return true;
+  else
+    evt.preventDefault();
+}
 
 function calcular()
 {
@@ -7,32 +14,29 @@ function calcular()
 	
 	var suma=parseInt(numero1)+parseInt(numero2);
 
+
 	if(numero1 == "" || numero2 == "" )
 	{
 
 		salida.innerHTML = "--";
 		document.getElementById("mensajes").innerHTML = '<div class="alert_num alert-danger"><p>No se ingreso ningun numero</p></div>';
 	}
-	else if (numero1 == 10 || numero2 == 10 ) 
+	else if (numero1 == 10 || numero2 == 10 || suma == 10) 
 	{
 		salida.innerHTML = "1";
 		//retorna 1
-		document.getElementById("mensajes").innerHTML = '<div class="alert_num alert-danger"><p>Escribiste un "10"</p></div>';
-	
-	}
-	else if	(suma == 10)
-	{
-		salida.innerHTML = "1";
-		//retorna 1
-		document.getElementById("mensajes").innerHTML = '<div class="alert_num alert-danger"><p>NLa suma es "10"</p></div>';
+		document.getElementById("mensajes").innerHTML = '<div class="alert_num alert-danger"><p>Ganaste</p></div>';
 	
 	}
 	else 
 	{
 		salida.innerHTML = "0";
 		//retorna 0
-		document.getElementById("mensajes").innerHTML = '<div class="alert_num alert-danger"><p>No hay problemas</p></div>';
+		document.getElementById("mensajes").innerHTML = '<div class="alert_num alert-danger"><p>Sigue intentado</p></div>';
 	
 	}
 
 }
+
+
+
